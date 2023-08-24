cd3e6bd323c6f401e987813666f458b2f848f99b
shaifali <shaifali02000@gmail.com>
2023-08-24 17:43:05 +0530

    new update

--- backend-blogApp/index.js
+++ backend-blogApp/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.send("Checking is working ")
 })
 app.use("/user",userRouter);
 app.use(auth);
